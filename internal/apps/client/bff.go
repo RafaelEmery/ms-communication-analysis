1c882bfb03dea0aea42626dc8ba6467e0984a60b
RafaelEmery <rafael.emerycade@gmail.com>
2023-11-11 14:46:44 -0300

    feat: bff interacting with grpc server

--- internal/apps/client/bff.go
+++ internal/apps/client/bff.go
@@ -2,14 +2,19 @@ package client
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	domain "github.com/RafaelEmery/performance-analysis-server/internal"
+	apps "github.com/RafaelEmery/performance-analysis-server/internal/apps/server"
 	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 const (
@@ -44,14 +49,22 @@ func (b *BFFApp) interactWithServer(c *fiber.Ctx) error {
 	if err := c.BodyParser(&data); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
+	o, err := b.handleMethods(c, data)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
 
-	return b.handleMethods(c, data)
+	return c.SendString(fmt.Sprintf("[%s] %d req for %s | %s", o, data.RequestQuantity, data.Resource, data.CommunicationMethod))
 }
 
-func (b *BFFApp) handleMethods(c *fiber.Ctx, data InteractionData) error {
+func (b *BFFApp) handleMethods(c *fiber.Ctx, data InteractionData) (string, error) {
+	log.Default().Printf("\n%s method on %s resource\n\n", data.CommunicationMethod, data.Resource)
+	var totalStart time.Time
+
 	if data.CommunicationMethod == httpMethod {
 		endpoint, method := b.getRequestData(data.Resource)
 
+		totalStart = time.Now()
 		for i := 0; i < data.RequestQuantity; i++ {
 			start := time.Now()
 			resp, err := doRequest(endpoint, method)
@@ -64,14 +77,31 @@ func (b *BFFApp) handleMethods(c *fiber.Ctx, data InteractionData) error {
 			elapsed := time.Since(start).String()
 			log.Default().Printf("[%d] %s - %s", resp.StatusCode, endpoint, elapsed)
 		}
-
-		return c.SendStatus(fiber.StatusOK)
 	}
 	if data.CommunicationMethod == grpcMethod {
-		return nil
+		conn, err := grpc.Dial(b.GRPCServerHost, grpc.WithInsecure())
+		if err != nil {
+			return "", err
+		}
+		defer conn.Close()
+
+		client := apps.NewProductHandlerClient(conn)
+		totalStart = time.Now()
+
+		for i := 0; i < data.RequestQuantity; i++ {
+			start := time.Now()
+			code, err := doProcedureCall(c.Context(), data.Resource, client)
+			if err != nil {
+				log.Default().Println(err)
+				continue
+			}
+
+			elapsed := time.Since(start).String()
+			log.Default().Printf("[%s] %s - %s", code, strings.ToUpper(string(data.Resource[0]))+data.Resource[1:], elapsed)
+		}
 	}
 
-	return nil
+	return time.Since(totalStart).String(), nil
 }
 
 func (b *BFFApp) getRequestData(resource string) (string, string) {
@@ -112,4 +142,47 @@ func doRequest(endpoint, method string) (*http.Response, error) {
 	}
 
 	return nil, fmt.Errorf("the method %s is not allowed", method)
+}
+
+func doProcedureCall(ctx context.Context, resource string, client apps.ProductHandlerClient) (string, error) {
+	var err error
+
+	switch resource {
+	case createResource:
+		var product domain.Product
+		fp := *product.Fake()
+
+		req := &apps.CreateProductRequest{
+			Name:              fp.Name,
+			Sku:               fp.SKU,
+			SellerName:        fp.SellerName,
+			Price:             float32(fp.Price),
+			AvailableDiscount: float32(fp.AvailableDiscount),
+			AvailableQuantity: int32(fp.AvailableQuantity),
+			SalesQuantity:     int32(fp.SalesQuantity),
+			Active:            fp.Active,
+		}
+
+		_, err = client.Create(ctx, req)
+	case reportResource:
+		req := &apps.EmptyRequest{}
+		_, err = client.Report(ctx, req)
+	case getByDiscountResource:
+		req := &apps.EmptyRequest{}
+		_, err = client.GetByDiscount(ctx, req)
+	}
+
+	return getProcedureCallCode(ctx), err
+}
+
+func getProcedureCallCode(ctx context.Context) string {
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !ok {
+		return "not_found"
+	}
+	statusCodes, ok := md["grpc-status"]
+	if !ok {
+		return "not_found"
+	}
+	return statusCodes[0]
 }
